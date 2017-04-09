5446af6cdc68d074e6b88234f1af2008a8f06d11
Majid Asgari <majid.asgari@gmail.com>
2017-04-09 18:32:32 +0430

    set language of template property

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
@@ -3,4 +3,10 @@ package ir.ac.iust.dml.kg.raw.utils
 object LanguageChecker {
    val englishRegex = Regex("[\\w \\d':;.,]+")
    fun isEnglish(text: String) = englishRegex.matches(text)
+
+   fun detectLanguage(text: String?) =
+         if (text == null) null
+         else {
+            if (isEnglish(text)) "en" else "fa"
+         }
 }
