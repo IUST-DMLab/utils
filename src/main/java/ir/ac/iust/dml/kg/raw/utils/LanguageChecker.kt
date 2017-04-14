a9cda2121e4a129ec4c2f0f3bef08b7b32bff017
Majid Asgari <majid.asgari@gmail.com>
2017-04-14 14:44:47 +0430

    write properties as triple

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
@@ -1,7 +1,7 @@
 package ir.ac.iust.dml.kg.raw.utils
 
 object LanguageChecker {
-   val englishRegex = Regex("[\\w \\d':;.,]+")
+   val englishRegex = Regex("[\\w \\d'-:;.,]+")
    fun isEnglish(text: String) = englishRegex.matches(text)
 
    fun detectLanguage(text: String?) =
