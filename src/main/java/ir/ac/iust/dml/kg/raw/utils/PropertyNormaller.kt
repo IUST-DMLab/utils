f0dd7a349eb81679e9bf293b5a4f20b9298c6568
Majid Asgari <majid.asgari@gmail.com>
2017-05-16 19:11:25 +0430

    Transformers class moving prefix loader to utils 0.2.0

---
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/PropertyNormaller.kt
@@ -0,0 +1,14 @@
+package ir.ac.iust.dml.kg.raw.utils
+
+object PropertyNormaller {
+  val DIGIT_END_REGEX = Regex("([ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی\\w: _]+)([۰۱۲۳۴۵۶۷۸۹\\d]+)")
+  fun removeDigits(property: String, removeUnderscore: Boolean = true, removeSpaces: Boolean = false): String {
+    var result =
+        if (removeUnderscore) property.replace("_", " ")
+        else if (removeSpaces) property.replace(" ", "_")
+        else property
+    if (DIGIT_END_REGEX.matches(result))
+      result = DIGIT_END_REGEX.matchEntire(result)!!.groups[1]!!.value.trim()
+    return result.toLowerCase()
+  }
+}
