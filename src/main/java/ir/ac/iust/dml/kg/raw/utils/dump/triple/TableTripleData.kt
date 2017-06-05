51006cfe780e74ba9c3b7b4d480fbe7e9b9c7705
Majid Asgari <majid.asgari@gmail.com>
2017-06-05 18:34:37 +0430

    table importer

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/dump/triple/TableTripleData.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/dump/triple/TableTripleData.kt
@@ -4,11 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class TableTripleData(
     @SerializedName("class")
-    var clazz: String? = null,
+    var ontologyClass: String? = null,
     var module: String? = null,
     @SerializedName("object")
     var objekt: String? = null,
     var predicate: String? = null,
+    var source: String? = null,
     var subject: String? = null,
     var version: String? = null
 )
