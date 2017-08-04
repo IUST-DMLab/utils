b9fa17336dcbb888bf23b88188d6c65ea34ca9d3
Majid Asgari <majid.asgari@gmail.com>
2017-08-04 16:47:46 +0430

    show entity data

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/LanguageChecker.kt
@@ -1,13 +1,19 @@
 package ir.ac.iust.dml.kg.raw.utils
 
 object LanguageChecker {
-   val englishRegex = Regex("[\\w \\d'-:;.,]+")
-   fun isEnglish(text: String) = englishRegex.matches(text)
+  val englishRegex = Regex("[\\w \\d'-:;.,]+")
+  fun isEnglish(text: String) = englishRegex.matches(text)
   fun isPersian(text: String) = !englishRegex.matches(text)
 
-   fun detectLanguage(text: String?) =
-         if (text == null) null
-         else {
-            if (isEnglish(text)) "en" else "fa"
-         }
+  fun detectLanguage(text: String?) =
+      if (text == null) null
+      else {
+        if (isEnglish(text)) "en" else "fa"
+      }
+
+  fun multiLanguages(text: String): Boolean {
+    if (englishRegex.matches(text)) return false
+    for (ch in text) if ((ch in 'A'..'Z') || (ch in 'a'..'z')) return true
+    return false
+  }
 }
