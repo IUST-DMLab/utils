d905ec122701a14c9cd15b0c181db3a0505622d6
Majid Asgari <majid.asgari@gmail.com>
2017-08-19 13:17:26 +0430

    moving module to Utils

---
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/Module.kt
@@ -0,0 +1,7 @@
+package ir.ac.iust.dml.kg.raw.utils
+
+enum class Module {
+  wiki, mapper_auto_labeling, web_table_extractor,
+  mapper_raw_entity_adder, raw_dependency_pattern, raw_rule_based,
+  raw_distant_supervision_logistic, raw_distant_supervision_deep
+}
