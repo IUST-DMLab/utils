3e66e63a055d76d92553898f6067ad3a220a56ea
Majid Asgari <majid.asgari@gmail.com>
2017-09-09 02:12:16 +0430

    remove deprecated classes and functions mandatory module and version

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/Module.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/Module.kt
@@ -3,5 +3,6 @@ package ir.ac.iust.dml.kg.raw.utils
 enum class Module {
   wiki, mapper_auto_labeling, web_table_extractor,
   raw_mapper_entity_adder, raw_dependency_pattern, raw_rule_based,
-  raw_distant_supervision_logistic, raw_distant_supervision_deep
+  raw_distant_supervision_logistic, raw_distant_supervision_deep,
+  manual, expert
 }
