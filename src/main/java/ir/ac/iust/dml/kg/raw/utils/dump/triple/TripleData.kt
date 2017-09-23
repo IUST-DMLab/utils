e53c9959ca7761dbe4a707f1575c5a99f24377f3
Majid Asgari <majid.asgari@gmail.com>
2017-09-23 15:05:15 +0330

    writing properties for numbered keys

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/dump/triple/TripleData.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/dump/triple/TripleData.kt
@@ -3,14 +3,18 @@ package ir.ac.iust.dml.kg.raw.utils.dump.triple
 import com.google.gson.annotations.SerializedName
 
 data class TripleData(
-      var source: String? = null,
-      var subject: String? = null,
-      var predicate: String? = null,
-      @SerializedName("template_name")
-      var templateNameFull: String? = null,
-      var templateName: String? = null,
-      @SerializedName("template_type")
-      var templateType: String? = null,
-      @SerializedName("object")
-      var objekt: String? = null
-)
+    var source: String? = null,
+    var subject: String? = null,
+    var predicate: String? = null,
+    @SerializedName("template_name")
+    var templateNameFull: String? = null,
+    var templateName: String? = null,
+    @SerializedName("template_type")
+    var templateType: String? = null,
+    @SerializedName("object")
+    var objekt: String? = null
+) {
+  override fun toString(): String {
+    return "($subject, $predicate, $objekt)"
+  }
+}
