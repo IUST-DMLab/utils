ec2c27e9aa249a2ef95398368bed21d2b0db983c
Majid Asgari <majid.asgari@gmail.com>
2017-10-08 20:31:19 +0330

    bug of mappings

--- src/main/java/ir/ac/iust/dml/kg/raw/utils/PropertyNormaller.kt
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/PropertyNormaller.kt
@@ -8,13 +8,14 @@ package ir.ac.iust.dml.kg.raw.utils
 
 object PropertyNormaller {
   val DIGIT_END_REGEX = Regex("([ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی\\w: _]+)([۰۱۲۳۴۵۶۷۸۹\\d]+)")
+  val DIGIT_END_REPLACE_REGEX = Regex("\\d*$")
   fun removeDigits(property: String, removeUnderscore: Boolean = true, removeSpaces: Boolean = false): String {
     var result =
         if (removeUnderscore) property.replace("_", " ")
         else if (removeSpaces) property.replace(" ", "_")
         else property
     if (DIGIT_END_REGEX.matches(result))
-      result = DIGIT_END_REGEX.matchEntire(result)!!.groups[1]!!.value.trim()
+      result = result.replace(DIGIT_END_REPLACE_REGEX, "")
     return result.toLowerCase()
   }
 }
