4e7abb062b366d839316baae9cdaf496a08815ed
Majid Asgari <majid.asgari@gmail.com>
2018-02-08 10:40:26 +0330

    uri checker

---
+++ src/main/java/ir/ac/iust/dml/kg/raw/utils/UriChecker.kt
@@ -0,0 +1,28 @@
+package ir.ac.iust.dml.kg.raw.utils
+
+import java.net.MalformedURLException
+import java.net.URI
+import java.net.URISyntaxException
+import java.net.URL
+
+@Suppress("MemberVisibilityCanPrivate", "unused")
+object UriChecker {
+
+  val checkedUris = mutableMapOf<String, Boolean>()
+
+  fun cachedCheckUri(uri: String) = checkedUris.getOrPut(uri, { checkUri(uri) })
+
+  fun checkUri(uri: String) =
+      try {
+        URL(uri)
+        URI(uri)
+        true
+      } catch (e: MalformedURLException) {
+        System.err.printf("Has not valid url $uri")
+        false
+      } catch (e: URISyntaxException) {
+        System.err.printf("Has not valid uri $uri")
+        false
+      }
+
+}
